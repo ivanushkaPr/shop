59c64c7728b56c3006455f3009dba1e1d2c8266d
Ivan Krasheninnikov <exitusletalis2008@yandex.ru>
2023-10-12 20:54:17 +0300

    Added integration with strapi.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,16 @@ export default defineNuxtConfig({
     modules: [
         '@nuxtjs/strapi',
         '@nuxtjs/google-fonts',
+        'nuxt-swiper',
     ],
-    strapi: {
-        url: 'http://localhost:1337'
+    strapi:
+{
+    devtools: true,
+        url: process.env.STRAPI_URL || 'http://127.0.0.1:1337',
+        prefix: '/api',
+        version: 'v4',
+        cookie: {},
+        cookieName: 'strapi_jwt'
     },
     googleFonts: {
         families: {
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,18 @@
+<script setup>
+const { find } = useStrapi()
+const response = await find('posts')
+</script>
+
 <template>
-  Hello
+  <div class="widgets">
+    <banner-slider/>
+    {{response}}
+  </div>
 </template>
+
+<style lang="scss">
+  .widgets {
+    padding: 24px 300px;
+    max-width: 1320px;
+  }
+</style>
